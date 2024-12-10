a50753868d4e5d72a083ff6eeeb371a1687f72b7
WAYNID <159060343+waynid@users.noreply.github.com>
2024-12-10 16:07:56 +0100

    Update code.py

--- code.py
+++ code.py
@@ -3,20 +3,20 @@ import os
 import usb_hid
 from keycode_win_fr import Keycode as badUSB
 from adafruit_hid.keyboard import Keyboard
-from keyboard_layout_win_fr import KeyboardLayout as KeyboardLayoutFR
-
-#  _____    __     ___      ____          _____      _    _ ______ _____  ______ 
-# |  __ \ /\\ \   / / |    / __ \   /\   |  __ \    | |  | |  ____|  __ \|  ____|
-# | |__) /  \\ \_/ /| |   | |  | | /  \  | |  | |   | |__| | |__  | |__) | |__   
-# |  ___/ /\ \\   / | |   | |  | |/ /\ \ | |  | |   |  __  |  __| |  _  /|  __|  
-# | |  / ____ \| |  | |___| |__| / ____ \| |__| |   | |  | | |____| | \ \| |____ 
-# |_| /_/    \_\_|  |______\____/_/    \_\_____/    |_|  |_|______|_|  \_\______|                                                                            
+from keyboard_layout_win_fr import KeyboardLayout as KeyboardLayoutFR                                                                         
 
 time.sleep(3)
 
 keyboard = Keyboard(usb_hid.devices)
 
 layout = KeyboardLayoutFR(keyboard)
+
+#  _____    __     ___      ____          _____      _    _ ______ _____  ______ 
+# |  __ \ /\\ \   / / |    / __ \   /\   |  __ \    | |  | |  ____|  __ \|  ____|
+# | |__) /  \\ \_/ /| |   | |  | | /  \  | |  | |   | |__| | |__  | |__) | |__   
+# |  ___/ /\ \\   / | |   | |  | |/ /\ \ | |  | |   |  __  |  __| |  _  /|  __|  
+# | |  / ____ \| |  | |___| |__| / ____ \| |__| |   | |  | | |____| | \ \| |____ 
+# |_| /_/    \_\_|  |______\____/_/    \_\_____/    |_|  |_|______|_|  \_\______|   
 
 keyboard.send(badUSB.WINDOWS, badUSB.R)
 
