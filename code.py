896816e405ae91927dfae55bdbe31ca72b5ba783
WAYNID <159060343+waynid@users.noreply.github.com>
2024-12-10 15:50:05 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,41 @@
+import time
+import os
+import usb_hid
+from keycode_win_fr import Keycode as badUSB
+from adafruit_hid.keyboard import Keyboard
+from keyboard_layout_win_fr import KeyboardLayout as KeyboardLayoutFR
+
+#  _____    __     ___      ____          _____      _    _ ______ _____  ______ 
+# |  __ \ /\\ \   / / |    / __ \   /\   |  __ \    | |  | |  ____|  __ \|  ____|
+# | |__) /  \\ \_/ /| |   | |  | | /  \  | |  | |   | |__| | |__  | |__) | |__   
+# |  ___/ /\ \\   / | |   | |  | |/ /\ \ | |  | |   |  __  |  __| |  _  /|  __|  
+# | |  / ____ \| |  | |___| |__| / ____ \| |__| |   | |  | | |____| | \ \| |____ 
+# |_| /_/    \_\_|  |______\____/_/    \_\_____/    |_|  |_|______|_|  \_\______|                                                                            
+
+time.sleep(3)
+
+keyboard = Keyboard(usb_hid.devices)
+
+layout = KeyboardLayoutFR(keyboard)
+
+keyboard.send(badUSB.WINDOWS, badUSB.R)
+
+time.sleep(1)
+
+layout.write("cmd")
+
+time.sleep(1)
+
+keyboard.send(badUSB.ENTER)
+
+time.sleep(1)
+
+layout.write("@echo off")
+    
+keyboard.send(badUSB.ENTER)
+
+layout.write("cls && echo Welcome to PICOBAD-USB ! THIS IS THE BASE CODE.")
+    
+keyboard.send(badUSB.ENTER)
+
+time.sleep(1)
